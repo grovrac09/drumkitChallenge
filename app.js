82ac61741868f9c660e5d6b22f56c194529046b7
Rachel Groves <grovrac09@gmail.com>
2021-11-02 15:52:50 +0000

    sound on click

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// ON KEYDOWN
 document.addEventListener("keydown", (event) => {
     if (event.key == "a" || event.key == "A") { 
        document.getElementById('boom').play();
@@ -26,5 +27,57 @@ document.addEventListener("keydown", (event) => {
     } else if (event.key == "l" || event.key == "L") { 
         document.getElementById('tom').play();
         return false;
+    } else {
+        return "Audio is not supported."
     }
 });
+
+// ON CLICK
+const aKey = document.getElementById("aKey");
+const sKey = document.getElementById("sKey");
+const dKey = document.getElementById("dKey");
+const fKey = document.getElementById("fKey");
+const gKey = document.getElementById("gKey");
+const hKey = document.getElementById("hKey");
+const jKey = document.getElementById("jKey");
+const kKey = document.getElementById("kKey");
+const lKey = document.getElementById("lKey");
+
+
+aKey.addEventListener("click", () => {
+    document.getElementById("boom").play();
+})
+
+sKey.addEventListener("click", () => {
+    document.getElementById("clap").play();
+})
+
+dKey.addEventListener("click", () => {
+    document.getElementById("hihat").play();
+})
+
+fKey.addEventListener("click", () => {
+    document.getElementById("kick").play();
+})
+
+gKey.addEventListener("click", () => {
+    document.getElementById("openhat").play();
+})
+
+hKey.addEventListener("click", () => {
+    document.getElementById("ride").play();
+})
+
+jKey.addEventListener("click", () => {
+    document.getElementById("snare").play();
+})
+
+kKey.addEventListener("click", () => {
+    document.getElementById("tink").play();
+})
+
+lKey.addEventListener("click", () => {
+    document.getElementById("tom").play();
+})
+
+
